7dbaacba7c2cb3ba8c1d18c647562e108249250a
ArmandoFC <fc.armando@outlook.com>
2024-04-28 20:02:00 -0600

    color to text

---
+++ day4.py
@@ -0,0 +1,18 @@
+myName = input("\033[32m" "What's your name? " "\033[33m")
+myEnemysName = input("\033[32mWhat's your worst enemy's name? " "\033[33m")
+mySuperPower = input("\033[32mWhat's your super power? " "\033[33m")
+myPlace = input("\033[32mWhere do you live? " "\033[33m")
+myFood = input("\033[32mWhat's your favorite food? " "\033[33m")
+print("\033[31m")
+print("=== Your Adventure Simulator ===")
+print(""" 
+      You"ll be asked a bunch of questions then 
+      we'll make you up an amazing story with YOU
+      as the star! 😜
+      """)
+print("\033[0m")
+print("Hellow\033[34m", myName, "\033[0mYour hability to", "\033[36m", mySuperPower)
+print("\033[0mwill make sure you never have to look at", "\033[35m", myEnemysName, "\033[0magaints.")
+print("Go eat\033[32m", myFood, "\033[0m" )
+print("you walk down the streets of\033[33m", myPlace, "\33[0m")
+print("use \33[36m", mySuperPower, "\033[0m for good and not evil!")
