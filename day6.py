9db6fabdd7c6c08a96f8168654ffe829d5c0c489
ArmandoFC <fc.armando@outlook.com>
2024-05-05 17:23:28 -0600

    what the elif is this?

---
+++ day6.py
@@ -0,0 +1,12 @@
+print ("MY LOGIN SYSTEM")
+print ("++++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+if username == "David" and password == "totallyNotBald":
+    print("Why hellow there David, what a lovely accent you have")
+elif username == "Susane" and password == "Su74nne":
+    print("Hey dere Susane!")
+elif username == "Armando" and password == "Sofi@":
+    print("Hola Armando!, bienvenido!")
+else:
+    print("Get away!")
