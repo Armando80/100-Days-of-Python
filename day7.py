60ce30d599980d78e8a2488588b03e52090cef9f
ArmandoFC <fc.armando@outlook.com>
2024-05-06 16:37:52 -0600

    Nesting - anidación 1

---
+++ day7.py
@@ -0,0 +1,12 @@
+tvShow = input("What's your favorite tv show? ")
+if tvShow == "pepa pig":
+    print("Ugh, why")
+    faveCharacter = input("Who is your favorite character? ")
+    if faveCharacter == "daddy pig":
+        print("Right answer")
+    else:
+        print("Nah, daddy pig's the greatest")
+elif tvShow == "paw patrol":
+    print("Aww, sad times")
+else:
+    print("yeah, that's cool and all...")
