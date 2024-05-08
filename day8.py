82b16625da7031e508507f64042f75c88b4918bb
ArmandoFC <fc.armando@outlook.com>
2024-05-07 21:25:23 -0600

    Challenge Day 8

---
+++ day8.py
@@ -0,0 +1,60 @@
+print("Wholesome Positivity Machine")
+print()
+name = input("Who are you? ")
+print()
+weekDay = input("What day of the week is it? ")
+print()
+favoriteAnime = input("What is your favorite anime? ")
+print()
+favoriteFood = input("What is your favorite food? ")
+print()
+favoriteColor = input("What is your favorite color? ")
+print()
+if name == "Armando" or name == "armando":
+  print("Hi Armando!")
+  if weekDay == "Monday" or weekDay == "monday":
+    print("It's going to be a great Monday!")
+  elif weekDay == "Tuesday" or weekDay == "tuesday":
+    print("What a wonderful Tuesday it is!")
+  elif weekDay == "Wednesday" or weekDay == "wednesday":
+    print("Happy Hump Day!")
+  elif weekDay == "Thursday" or weekDay == "thursday":
+    print(name, "your week is almost over!")
+  elif weekDay == "Friday" or weekDay == "friday":
+    print(name, "It's FRIDAY!")
+  else:
+    print("I hope you have a great weekend!")
+elif name == "Juan" or name == "juan":
+  print("Hi Juan!")
+  if weekDay == "Monday" or weekDay == "monday":
+    print("It's going to be a great Monday!")
+  elif weekDay == "Tuesday" or weekDay == "tuesday":
+    print("You look great in that color today!")
+  elif weekDay == "Wednesday" or weekDay == "wednesday":
+    print("You look chipper today!")
+  elif weekDay == "Thursday" or weekDay == "thursday":
+    print(name, "you are doing a great job!")
+  elif weekDay == "Friday" or weekDay == "friday":
+    print(name, "it's FRIDAY!")
+  else:
+    print("I hope you have a great weekend!")
+else:
+  print("get out here!")
+if favoriteAnime == "One Piece" or favoriteAnime == "one piece":
+  print("That's my favorite anime too!")
+elif favoriteAnime == "Naruto" or favoriteAnime == "naruto":
+  print("That's my second favorite anime!")
+else:
+  print("I haven't heard of that anime before!")
+if favoriteFood == "Pizza" or favoriteFood == "pizza":
+  print("That's my favorite food too!")
+elif favoriteFood == "Burgers" or favoriteFood == "burgers":
+  print("That's my second favorite food!")
+else:
+  print("I haven't heard of that food before!")
+if favoriteColor == "Blue" or favoriteColor == "blue":
+  print("That's my favorite color too!")
+elif favoriteColor == "Red" or favoriteColor == "red":
+    print("That's my second favorite color!")
+else:
+  print("I haven't heard of that color before!")
