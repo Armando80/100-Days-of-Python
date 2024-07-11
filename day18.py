158915eaecac8173abca6de8972253ea60904eb3
ArmandoFC <fc.armando@outlook.com>
2024-07-11 16:30:41 -0600

    Guess the number GessingGame

---
+++ day18.py
@@ -0,0 +1,22 @@
+import getpass
+print("One-Million-To-One")
+myNumber = getpass.getpass(prompt='Give me the number: ')
+count = 0
+while True:
+    yourNumber = input("What is your gess?: ")
+    count += 1
+    if myNumber == yourNumber:
+        print("Correct")
+        break
+    elif myNumber > yourNumber:
+        print("Too low")
+    elif myNumber < yourNumber:
+        print("Too higth")
+    elif myNumber > 1000000:
+        print("your number is too high than 1 million")
+        exit()
+    elif myNumber < 0:
+        exit()
+    else:
+        continue
+print("It took you ", count, "guesses to get it correct!")
